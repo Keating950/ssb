28dac1f21e68ed40f75a62c6eb6f1a422c071607
Keating Reid <keating.reid@protonmail.com>
2021-06-23 13:17:57 -0400

    initial commit

---
+++ src/bookmarks.rs
@@ -0,0 +1,123 @@
+use once_cell::sync::OnceCell;
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    default::Default,
+    fmt,
+    fs::{read_to_string, OpenOptions},
+    io::prelude::*,
+    path::Path,
+};
+use xdg::BaseDirectories;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Bookmarks {
+    #[serde(flatten)]
+    entries: HashMap<String, String>,
+}
+
+impl fmt::Display for Bookmarks {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let width = self
+            .entries
+            .keys()
+            .map(String::len)
+            .max()
+            .unwrap_or_default();
+        for (i, (k, v)) in self.entries.iter().enumerate() {
+            if i < self.entries.len() - 1 {
+                writeln!(f, "{:>width$} -> {}", k, v, width = width)?;
+            } else {
+                write!(f, "{:>width$} -> {}", k, v, width = width)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Bookmarks {
+    const FILENAME: &'static str = "bookmarks.json";
+
+    pub fn new() -> anyhow::Result<Bookmarks> {
+        match Bookmarks::base_dirs().find_data_file(Bookmarks::FILENAME) {
+            Some(f) => {
+                let text = read_to_string(f)?;
+                let b: Bookmarks = serde_json::from_str(&text)?;
+                Ok(b)
+            }
+            None => Ok(Bookmarks::default()),
+        }
+    }
+
+    pub fn insert<S: Into<String>>(&mut self, key: S, val: S) {
+        self.entries.insert(key.into(), val.into());
+    }
+
+    pub fn remove(&mut self, key: &str) -> Option<String> {
+        self.entries.remove(key)
+    }
+
+    pub fn get(&self, key: &str) -> Option<&str> {
+        self.entries.get(key).map(|s| s.as_str())
+    }
+
+    pub fn save(&self) -> anyhow::Result<()> {
+        let save_path = match Bookmarks::base_dirs().find_data_file(Bookmarks::FILENAME) {
+            Some(p) => p,
+            None => Bookmarks::base_dirs().place_data_file(Bookmarks::FILENAME)?,
+        };
+        self.save_to_path(save_path)
+    }
+
+    fn base_dirs() -> &'static BaseDirectories {
+        static XDG_ENTRIES: OnceCell<BaseDirectories> = OnceCell::new();
+        XDG_ENTRIES.get_or_init(|| BaseDirectories::with_prefix(env!("CARGO_PKG_NAME")).unwrap())
+    }
+
+    fn save_to_path<T: AsRef<Path>>(&self, path: T) -> anyhow::Result<()> {
+        let mut f = OpenOptions::new().write(true).create(true).open(path)?;
+        f.write(serde_json::to_string(self)?.as_bytes())?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs::read_to_string;
+
+    macro_rules! assert_ok {
+        ($v:ident) => {
+            assert!($v.is_ok(), "{}", $v.unwrap_err())
+        };
+        ($e:expr) => {{
+            let tmp = $e;
+            assert!(tmp.is_ok(), "{}", tmp.unwrap_err())
+        }};
+    }
+
+    #[test]
+    fn test_serialize() {
+        let mut b = Bookmarks::new();
+        b.insert("hello", "world");
+        b.insert("a", "b");
+        b.insert("c", "d");
+        let json = serde_json::to_string(&b);
+        assert_ok!(json);
+        println!("{}", json.unwrap());
+    }
+
+    #[test]
+    fn test_deserialize() {
+        let text = read_to_string("test_data/test_data.json").unwrap();
+        let b: Result<Bookmarks, _> = serde_json::from_str(&text);
+        assert_ok!(b)
+    }
+
+    #[test]
+    fn test_save_to_path() {
+        let text = read_to_string("test_data/test_data.json").unwrap();
+        let b: Result<Bookmarks, _> = serde_json::from_str(&text);
+        assert_ok!(b.unwrap().save_to_path("test_data/test_save.json"))
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+mod bookmarks;
+
+use crate::bookmarks::Bookmarks;
+use std::ffi::CStr;
+
+const LIST_SUBCOMMAND: &'static str = "-l";
+const REMOVE_SUBCOMMAND: &'static str = "-r";
+const ADD_SUBCOMMAND: &'static str = "-a";
+const KEY_ARGNAME: &'static str = "KEY";
+const VALUE_ARGNAME: &'static str = "VALUE";
+
+fn main() {
+    if let Err(e) = try_main() {
+        eprintln!("{}", e);
+        std::process::exit(1)
+    }
+}
+
+fn try_main() -> anyhow::Result<()> {
+    let app = make_arg_parser().get_matches();
+    let mut bookmarks = Bookmarks::new()?;
+    match app.subcommand() {
+        (LIST_SUBCOMMAND, _) => {
+            println!("{}", bookmarks)
+        }
+        (REMOVE_SUBCOMMAND, Some(args)) => {
+            let k = args.value_of(KEY_ARGNAME).unwrap();
+            if bookmarks.remove(k).is_none() {
+                return Err(anyhow::anyhow!(r#"Key "{}" not found."#, k));
+            }
+            bookmarks.save()?;
+        }
+        (ADD_SUBCOMMAND, Some(args)) => {
+            bookmarks.insert(
+                args.value_of(KEY_ARGNAME).unwrap(),
+                args.value_of(VALUE_ARGNAME).unwrap(),
+            );
+            bookmarks.save()?;
+        }
+        ("", None) => {
+            let key = app.value_of(KEY_ARGNAME).unwrap();
+            let val = bookmarks
+                .get(key)
+                .ok_or_else(|| anyhow::anyhow!(anyhow::anyhow!(r#"Key "{}" not found."#, key)))?;
+            start_ssh(val);
+        }
+        _ => unreachable!(),
+    }
+    Ok(())
+}
+
+fn make_arg_parser() -> clap::App<'static, 'static> {
+    use clap::{
+        app_from_crate, crate_authors, crate_description, crate_name, crate_version, AppSettings,
+        Arg, SubCommand,
+    };
+    let list_subcommand = SubCommand::with_name(LIST_SUBCOMMAND).help("List bookmarks and exit.");
+    let rm_subcommand = SubCommand::with_name(REMOVE_SUBCOMMAND)
+        .help("Remove a bookmark.")
+        .arg(Arg::with_name(KEY_ARGNAME));
+    let add_subcommand = SubCommand::with_name(ADD_SUBCOMMAND)
+        .help("Add a bookmark. Arguments should be in the format KEY USER@IP.")
+        .arg(Arg::with_name(KEY_ARGNAME))
+        .arg(Arg::with_name(VALUE_ARGNAME));
+    app_from_crate!()
+        .setting(AppSettings::ArgsNegateSubcommands)
+        .setting(AppSettings::SubcommandsNegateReqs)
+        .arg(
+            Arg::with_name(KEY_ARGNAME)
+                .help("Bookmark to connect to.")
+                .required(true),
+        )
+        .subcommands([list_subcommand, rm_subcommand, add_subcommand])
+}
+
+fn start_ssh(addr: &str) {
+    const SSH_CMD_BYTES: [u8; 4] = ['s' as u8, 's' as u8, 'h' as u8, 0];
+    let addr_bytes: Vec<u8> = addr.as_bytes().iter().chain(&[0]).map(|c| *c).collect();
+    unsafe {
+        let ssh_cmd = CStr::from_bytes_with_nul_unchecked(&SSH_CMD_BYTES);
+        let addr_cstr = CStr::from_bytes_with_nul_unchecked(&addr_bytes);
+        // NOTE: For whatever reason, likely a bug in the nix crate, the first element of execvp's
+        // argv is ignored. This is why the variable tmp is being used here.
+        let tmp = CStr::from_bytes_with_nul_unchecked(&[0]);
+        nix::unistd::execvp::<&CStr>(ssh_cmd, &[tmp, addr_cstr]);
+    }
+}
