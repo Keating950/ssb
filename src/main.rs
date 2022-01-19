f1884512c02a9013e749e8a3e170c20c24b79e10
Keating Reid <keating.reid@protonmail.com>
2022-01-19 10:46:04 -0500

    upgrade to clap 3.0.10

--- src/main.rs
+++ src/main.rs
@@ -25,17 +25,17 @@ fn try_main() -> anyhow::Result<()> {
     let app = make_arg_parser().get_matches();
     let mut bookmarks = Bookmarks::load()?;
     match app.subcommand() {
-        (LIST_SUBCOMMAND, _) => {
+        Some((LIST_SUBCOMMAND, _)) => {
             println!("{}", bookmarks)
         }
-        (REMOVE_SUBCOMMAND, Some(args)) => {
+        Some((REMOVE_SUBCOMMAND, args)) => {
             let k = args.value_of(KEY_ARGNAME).unwrap();
             if bookmarks.remove(k).is_none() {
                 return Err(anyhow::anyhow!(r#"Key "{}" not found."#, k));
             }
             bookmarks.save()?;
         }
-        (ADD_SUBCOMMAND, Some(args)) => {
+        Some((ADD_SUBCOMMAND, args)) => {
             let key = args.value_of(KEY_ARGNAME).unwrap();
             if let Some(bmark) = bookmarks.get(key) {
                 let mut buf = String::default();
@@ -57,7 +57,7 @@ fn try_main() -> anyhow::Result<()> {
             );
             bookmarks.save()?;
         }
-        ("", None) => {
+        None => {
             let key = app.value_of(KEY_ARGNAME).unwrap();
             let val = bookmarks
                 .remove(key)
@@ -69,40 +69,35 @@ fn try_main() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn make_arg_parser() -> clap::App<'static, 'static> {
-    use clap::{
-        app_from_crate, crate_authors, crate_description, crate_name, crate_version, AppSettings,
-        Arg, SubCommand,
-    };
-    let list_subcommand = SubCommand::with_name(LIST_SUBCOMMAND).about("List bookmarks and exit.");
-    let rm_subcommand = SubCommand::with_name(REMOVE_SUBCOMMAND)
+fn make_arg_parser() -> clap::App<'static> {
+    use clap::{app_from_crate, App, AppSettings, Arg};
+    let list_subcommand = App::new(LIST_SUBCOMMAND).about("List bookmarks and exit.");
+    let rm_subcommand = App::new(REMOVE_SUBCOMMAND)
         .about("Remove a bookmark.")
-        .arg(Arg::with_name(KEY_ARGNAME).required(true));
-    let add_subcommand = SubCommand::with_name(ADD_SUBCOMMAND)
+        .arg(Arg::new(KEY_ARGNAME).required(true));
+    let add_subcommand = App::new(ADD_SUBCOMMAND)
         .about("Add a bookmark. Arguments should be in the format KEY USER@IP.")
         .arg(
-            Arg::with_name(KEY_ARGNAME)
+            Arg::new(KEY_ARGNAME)
                 .help("Key to use for the new bookmark.")
                 .required(true),
         )
         .arg(
-            Arg::with_name(ADDR_ARGNAME)
+            Arg::new(ADDR_ARGNAME)
                 .help("Addresses should be in the format USER@IP.")
                 .required(true),
         )
         .arg(
-            Arg::with_name(ARGS_ARGNAME)
+            Arg::new(ARGS_ARGNAME)
                 .help("Custom arguments to pass to the ssh command, e.g. '-i ~/.ssh/id_rsa'.")
                 .last(true),
         );
     app_from_crate!()
-        .settings(&[
-            AppSettings::ArgsNegateSubcommands,
-            AppSettings::SubcommandsNegateReqs,
-            AppSettings::DisableHelpSubcommand,
-        ])
+        .global_setting(AppSettings::ArgsNegateSubcommands)
+        .global_setting(AppSettings::SubcommandsNegateReqs)
+        .global_setting(AppSettings::DisableHelpSubcommand)
         .arg(
-            Arg::with_name(KEY_ARGNAME)
+            Arg::new(KEY_ARGNAME)
                 .help("Bookmark to connect to.")
                 .required(true),
         )
@@ -118,7 +113,6 @@ fn start_ssh(b: Bookmark) -> Result<core::convert::Infallible, nix::Error> {
     nix::unistd::execvp(ssh_cmd, &args)
 }
 
-
 #[cfg(test)]
 #[macro_export]
 macro_rules! assert_ok {
@@ -131,3 +125,20 @@ macro_rules! assert_ok {
     }};
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_arg_parser() {
+        let args = [
+            vec!["ssb", "foobar_key"],
+            vec!["ssb", "-a", "foobar_key", "baz@192.168.0.1"],
+            vec!["ssb", "-l"],
+            vec!["ssb", "-rm", "foobar_key"],
+        ];
+        for a in args {
+            assert_ok!(make_arg_parser().try_get_matches_from(a));
+        }
+    }
+}
